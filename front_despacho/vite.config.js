00fd5e9a9fd7e5bc31f748817a884a95622ff1d9
EliasCarcamo18 <el.carcamo@duocuc.cl>
2026-05-17 21:03:07 -0400

    fix: forzar buscador de dockerfile e id de instancia

--- front_despacho/vite.config.js
+++ front_despacho/vite.config.js
@@ -5,12 +5,22 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Permite que se acceda externamente en la red 
+    port: 5173, // O el puerto que use tu frontend (ej: 80, 3000, 5173)
     proxy: {
-      '/api': {
-        target: 'https://qic534o8o0.execute-api.us-east-1.amazonaws.com',
+      // 📦 Redirección para Despachos
+      '/api/despacho': {
+        target: 'http://10.0.136.18:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api\/despacho/, '')
+      },
+      // 💰 Redirección para Ventas
+      '/api/venta': {
+        target: 'http://10.0.136.18:5001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/venta/, '')
       }
     }
   }
 })
+
